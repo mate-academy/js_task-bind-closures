bcc554671cc93237473d9315d21a01c68fbf5a4c
Ihor Stavratii <igor.stavratiy@gmail.com>
2019-11-25 15:23:25 +0200

    solution

--- src/bind.js
+++ src/bind.js
@@ -18,8 +18,16 @@
  *
  * @return {Function}
  */
-function bind(callback) {
-  // write code here
+function bind(callback, ...args) {
+  let listOfParams;
+
+  const device = function(...params) {
+    listOfParams = [ ...args, ...params ];
+
+    return listOfParams;
+  };
+
+  return device;
 }
 
 module.exports = bind;
