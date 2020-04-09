fb3e455b67f33da0e6ee56bba45af283490b751c
Vladimir-Beliy <vovaazick@gmail.com>
2020-04-09 18:30:31 +0300

    solution

--- src/bind.js
+++ src/bind.js
@@ -18,8 +18,14 @@
  *
  * @return {Function}
  */
-function bind(callback) {
+function bind(callback, ...args1) {
   // write code here
+
+  const device = (...args2) => {
+    return callback(...args1, ...args2);
+  };
+
+  return device;
 }
 
 module.exports = bind;
