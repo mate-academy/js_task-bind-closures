1872ac0d0a4eca24c09546f0dd4145d9f1f26568
BorisovecViktor <viktorborisovets@gmail.com>
2020-04-10 13:08:41 +0300

    solution

--- src/bind.js
+++ src/bind.js
@@ -18,8 +18,10 @@
  *
  * @return {Function}
  */
-function bind(callback) {
-  // write code here
+function bind(callback, ...argsDefault) {
+  return function(...argsToAdd) {
+    return callback(...argsDefault, ...argsToAdd);
+  };
 }
 
 module.exports = bind;
