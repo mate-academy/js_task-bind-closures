1862ab456380b688a71c103cdc9fb18423d74b06
Campari229 <KennyKoin@gmail.com>
2020-06-08 19:15:58 +0300

    Solution

--- src/bind.js
+++ src/bind.js
@@ -18,8 +18,10 @@
  *
  * @return {Function}
  */
-function bind(callback) {
-  // write code here
+function bind(callback, ...args) {
+  return function(...newArgs) {
+    return callback(...args, ...newArgs);
+  };
 }
 
 module.exports = bind;
